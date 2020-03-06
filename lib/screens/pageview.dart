fe568c24b4650c2080de5f7e2d814058323405aa
srjpnd <srjpnd96@gmail.com>
2020-03-06 22:37:55 +0530

    pageviews.dart file has been added !

---
+++ lib/screens/pageview.dart
@@ -0,0 +1,11 @@
+import "package:flutter/material.dart";
+
+class pageviews extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(child: Text("Pageviews",style: TextStyle(fontSize: 20.0),),),
+      
+    );
+  }
+}
